1a39e9db64c5bb457ec42edf1193d232eced2ad0
hoangdh14 <59603737+hoangdh14@users.noreply.github.com>
2020-02-25 16:40:40 +0700

    add some files

---
+++ myserver.js
@@ -0,0 +1,15 @@
+var express=require('express');
+var app=express();
+app.set('view engine','jade');
+var port = process.env.PORT || 3000;
+
+app.get('/',function(req,res)
+{
+    res.render('index',{title:'HoangDH',message:'Welcome HoangDH',"book": {"name": "Hello", "price": 12.99}})
+});
+
+app.get('/test',function(req,res)
+{
+    res.send('Hello test!');
+});
+var server=app.listen(port,function() {});
